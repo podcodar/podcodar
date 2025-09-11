ebff004296e270b012283522ce29c07512667248
Marco Antônio <ma.souza.junior@gmail.com>
2025-09-11 15:11:55 -0300

    fea: add github action to validate courses

--- scripts/validate.ts
+++ scripts/validate.ts
@@ -1,59 +1,48 @@
-#! which deno
-import { z } from "npm:zod";
+import { z } from "zod";
 
 const locale = ["en", "es", "fr", "de"];
 const technologies = [
-	"phoenix",
-	"liveview",
-	"ecto",
-	"channels",
-	"deployment",
-	"api",
-	"testing",
-	"fullstack",
+  "phoenix",
+  "liveview",
+  "ecto",
+  "channels",
+  "deployment",
+  "api",
+  "testing",
+  "fullstack",
 ];
 
 const CourseSchema = z.object({
-	title: z.string().min(5, "Title must be at least 5 characters long"),
-	link: z.string().url("Link must be a valid URL"),
-	description: z
-		.string()
-		.min(20, "Description must be at least 20 characters long"),
-	// locale with options
-	locale: z.enum(locale, {
-		errorMap: () => ({ message: `Locale must be one of: ${locale.join(",")}` }),
-	}),
-	technologies: z
-		.array(
-			z.enum(technologies, {
-				errorMap: () => ({
-					message: `Technology must be one of: ${technologies.join(",")}`,
-				}),
-			}),
-		)
-		.min(1, "At least one technology must be specified"),
+  title: z.string().min(5, "Title must be at least 5 characters long"),
+  link: z.url("Link must be a valid URL"),
+  description: z
+    .string()
+    .min(20, "Description must be at least 20 characters long"),
+  // locale with options
+  locale: z.enum(locale),
+  technologies: z
+    .array(z.enum(technologies))
+    .min(1, "At least one technology must be specified"),
 });
 
 const CourseListSchema = z
-	.array(CourseSchema)
-	.min(1, "Course list cannot be empty");
+  .array(CourseSchema)
+  .min(1, "Course list cannot be empty");
 
-const args = Deno.args();
-
-if (args.length !== 1) {
-	console.error(
-		"Usage: deno run --allow-read scripts/validate.ts <path-to-json>",
-	);
-	Deno.exit(1);
+if (Deno.args.length !== 1) {
+  console.error(
+    "Usage: deno run --allow-read scripts/validate.ts <path-to-json>",
+  );
+  Deno.exit(1);
 }
 
-const filePath = args[0];
+const filePath = Deno.args[0];
 const data = await Deno.readTextFile(filePath);
 
 const courses = CourseListSchema.safeParse(JSON.parse(data));
 if (!courses.success) {
-	console.error("Validation errors:", courses.error.errors);
-	Deno.exit(1);
+  console.error("Validation errors:", courses.error);
+  Deno.exit(1);
 }
 
 console.log("All courses are valid!");
