61c4c1383a85f51a3b33c43f3ac89d7126d6df8b
Marco Antônio <ma.souza.junior@gmail.com>
2025-11-04 18:56:53 -0300

    enh: update deps

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Podcodar.MixProject do
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.4.1", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.2.0",
